6c2d2a5cfc5a136aed21a26cecfe0e711a7bea77
nicolas massuyes <nicos1903@hotmail.fr>
2013-08-19 17:17:40 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Nicos
+
+this is the sample application for
+[*nicos: l'app qui dechire*](http://nicos.com)
+
+by [nicolas massuyes]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nicos::Application.config.secret_token = 'ce84710e2e8e71e4e30a2c6c8af117dc851b679905365e254b58159c1e2f0c80d6f137188184c1a627341443cd23b4dce121bd3a962de5dd1b7635b3a1bc0b95'
